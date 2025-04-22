6361ae20eda2d79fd50f383761a2caf8ab32ec98
Benjamin Marchand <bmarchand@ippon.fr>
2025-04-22 10:43:29 +0200

    Initial commit

---
+++ src/main/java/com/bmarchand/konfi/KonfiApplication.java
@@ -0,0 +1,13 @@
+package com.bmarchand.konfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonfiApplication.class, args);
+	}
+
+}
